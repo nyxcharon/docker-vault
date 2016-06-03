b5018a39f1278e9f4112d3ef23d8081eb85208be
bmartin <barry.martin@move.com>
2016-06-03 11:51:11 -0400

    Move to using dockerize for config, upgrade vault to 0.5.3

--- Dockerfile
+++ Dockerfile
@@ -13,7 +13,7 @@ RUN tar -C /usr/local/bin -xzvf dockerize-linux-amd64-v0.2.0.tar.gz
 # Listener API tcp port
 EXPOSE 8200
 ADD vault /usr/local/bin/vault
-ADD consul.hcl /etc/consul.hcl
+ADD consul.tmpl /config/consul.tmpl
 ADD start.sh /start.sh
 RUN touch /var/log/vault_audit.log
 
--- start.sh
+++ start.sh
@@ -6,24 +6,5 @@ if [ -f /config/vault.hcl ]; then
   /usr/local/bin/dockerize -stdout /var/log/vault_audit.log -poll /usr/local/bin/vault server -config=/config/vault.hcl
 else
   echo "Using default config for vault"
-
-  if [ ! -z "$CONSUL_ADDR" ]; then
-    sed -i "s~CONSUL_IP~$CONSUL_ADDR~g" /etc/consul.hcl
-  else
-    echo "CONSUL_ADDR must be set"
-    exit 1
-  fi
-
-  #If the VAULT_IP variable is set, set the advertise address, otherwise remove the line
-  if [ -z "$VAULT_IP" ]; then
-    sed -i '/SED_IP/d' /etc/consul.hcl
-  else
-    sed -i "s~SED_IP~$VAULT_IP~g" /etc/consul.hcl
-  fi
-
-  if [ ! -z "$CONSUL_PATH" ]; then
-    sed -i "s/vault-prod/$CONSUL_PATH/g" /etc/consul.hcl
-  fi
-
-  /usr/local/bin/dockerize -stdout /var/log/vault_audit.log -poll /usr/local/bin/vault server -config=/etc/consul.hcl
+  /usr/local/bin/dockerize -template /config/consul.tmpl:/config/consul.hcl -stdout /var/log/vault_audit.log -poll /usr/local/bin/vault server -config=/config/consul.hcl
 fi
